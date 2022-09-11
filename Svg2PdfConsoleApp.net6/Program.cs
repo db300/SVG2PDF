3951d89125c1b98244626c6098bbf23a60e727fc
DavidHawkes <db300@163.com>
2022-09-11 23:54:00 +0800

    Update Program.cs

--- Svg2PdfConsoleApp.net6/Program.cs
+++ Svg2PdfConsoleApp.net6/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length > 0)
+            {
+                Console.Out.WriteLine(args[0]);
+                var svg = args[0];
+                var pdf = $"{args[0]}.pdf";
+                iHawkSvg2PdfLibrary.Svg2Pdf.Convert(svg, pdf);
+                Console.Out.WriteLine(pdf);
+            }
+            else
+            {
+                var svgs = new List<string>
+                {
+                    @"C:\GitHub\FontStudioV1\FontStudio\bin\Debug\tmp\000.svg"
+                };
+                var pdf = "000.pdf";
+                iHawkSvg2PdfLibrary.Svg2Pdf.Convert(svgs, pdf);
+            }
+            Console.ReadLine();
         }
     }
 }
