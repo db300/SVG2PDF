ad1d667d2173297f026b699272a8117e5de5581b
DavidHawkes <db300@163.com>
2022-09-11 17:35:04 +0800

    新增SVG转PDF控制台应用.net6版本项目

---
+++ Svg2PdfConsoleApp.net6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Svg2PdfConsoleApp.net6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
